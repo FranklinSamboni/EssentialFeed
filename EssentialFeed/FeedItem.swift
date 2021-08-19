16655b3d3e5d6f62c92862669426e1ca92d81db7
Franklin Samboni Castillo <franklin.samboni@globant.com>
2021-08-18 20:15:33 -0500

    Create feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Franklin Samboni Castillo on 18/08/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
